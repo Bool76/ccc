c2765b1bd19ad0a0c7cec37ca7ea4215423cf73c
Ian Gall <ian.gall@scotts.com>
2018-11-18 16:03:10 -0500

    Update to index for google verification

--- public/index.php
+++ public/index.php
@@ -1,188 +1,190 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-110066838-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+  <!-- Site Verification per Google -->
+  <meta name="google-site-verification" content="O86fDgNewQp2rB-frirjCeeP7AE3O0ULKhPRMZP5fzI" />
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-110066838-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
 
-  gtag('config', 'UA-110066838-1');
-</script>
-	
-<title>Creative Child Care INC</title>
+    gtag('config', 'UA-110066838-1');
+  </script>
 
-	<meta name="keywords" content="childcare, child care, columbus ohio, columbus oh, early childhood education.">
-	<meta name="description" content="Creative Child Care is a highly rated early educational childcare provider in the Columbus Ohio area.">
+  <title>Creative Child Care INC</title>
+
+  <meta name="keywords" content="childcare, child care, columbus ohio, columbus oh, early childhood education.">
+  <meta name="description" content="Creative Child Care is a highly rated early educational childcare provider in the Columbus Ohio area.">
 
   <!--Load CSS Files -->
   <link rel="stylesheet" type="text/css" href="css/site.css">
   <link rel="stylesheet" type="text/css" href="css/index.css">
-	<link rel="stylesheet" type="text/css" href="css/homeMarquees.css">
+  <link rel="stylesheet" type="text/css" href="css/homeMarquees.css">
 
   <!--Load Google Fonts -->
-		<?php
-			include("new-webFonts.php")
-		?>
+  <?php
+  include("new-webFonts.php")
+  ?>
 
-		<!-- Load JS Files -->
-		<?php
-			include("js-files.php")
-		?>
+  <!-- Load JS Files -->
+  <?php
+  include("js-files.php")
+  ?>
 
-		<script src="js/center-slider.js"></script>
+  <script src="js/center-slider.js"></script>
 
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
 <body>
 <?php
-	include("header.php")
+include("header.php")
 ?>
 <div id="ccc-body-container">
-		   <div class="clearfix"></div>
-					<div id="image-slider-wrapper">
-						 <div class="slider">
-							  <div class="slider-img one" id="1"/>
-								   <div class="landingMarquee-message">
-												Building your child's future... creatively.
-											</div>
-								 </div>
-                <div class="slider-img four" id="4"/>
-                  <div class="ode-message-container">
-                    <h4>
-                      It's not too late,
-                      enroll your 4 year old
-                      in our free 1/2 day
-                      preschool program
-                      TODAY!
-                    </h4>
-                  </div>
-                    <div class="ode-left-col">
-                      <p class="ode-blurbs">
-                        <span>Call</span> or walk-in to secure your spot today!
-                      </p>
-                      <img src="/images/marquees/ode/ode-boy-2.png">
-                    </div>
-                    <div class="ode-mid-col">
-                      <img src="/images/marquees/ode/ode-boy-1.png">
-                      <p class="ode-blurbs">
-                        <span>Free</span> high quality program if you qualify
-                      </p>
-                    </div>
-                    <div class="ode-right-col">
-                      <div class="call-your-container">
-                        <img src="/images/marquees/ode/call-your-closest.png">
-                      </div>
-                      <p class="ode-blurbs">
-                        <span>Prepare</span> your child for Kindergarten now
-                      </p>
-                      <img src="/images/marquees/ode/ode-girls-1.png">
-                    </div>
+  <div class="clearfix"></div>
+  <div id="image-slider-wrapper">
+    <div class="slider">
+      <div class="slider-img one" id="1"/>
+      <div class="landingMarquee-message">
+        Building your child's future... creatively.
+      </div>
+    </div>
+    <div class="slider-img four" id="4"/>
+    <div class="ode-message-container">
+      <h4>
+        It's not too late,
+        enroll your 4 year old
+        in our free 1/2 day
+        preschool program
+        TODAY!
+      </h4>
+    </div>
+    <div class="ode-left-col">
+      <p class="ode-blurbs">
+        <span>Call</span> or walk-in to secure your spot today!
+      </p>
+      <img src="/images/marquees/ode/ode-boy-2.png">
+    </div>
+    <div class="ode-mid-col">
+      <img src="/images/marquees/ode/ode-boy-1.png">
+      <p class="ode-blurbs">
+        <span>Free</span> high quality program if you qualify
+      </p>
+    </div>
+    <div class="ode-right-col">
+      <div class="call-your-container">
+        <img src="/images/marquees/ode/call-your-closest.png">
+      </div>
+      <p class="ode-blurbs">
+        <span>Prepare</span> your child for Kindergarten now
+      </p>
+      <img src="/images/marquees/ode/ode-girls-1.png">
+    </div>
 
-                </div>
-                <div class="slider-img two" id="2"/>
-                  <a id="jobs-marquee-applyNow" href="jobOpportunities.php">
-                    <p>
-                      Apply Now
-                    </p>
-                  </a>
-                  <div id="jobs-marquee-joinOur">
-                    <p>
-                      <a href="jobOpportunities.php">Join Our Team</a>
-                    </p>
-                  </div>
-                  <div id="jobs-marquee-ourCompany-desktop">
-                    <p id="jobs-marquee-ourCompany-desktopCopy">
-                      Our <strong>company</strong> is looking for energetic, friendly, professionals with a passion for teaching
-                      and working  with young children.
-                    </p>
-                  </div>
-											<div id="jobs-marquee-ourCompany-mobile">
-												<p id="jobs-marquee-ourCompany-mobileCopy">
-													Our <strong>company</strong> is looking for energetic, friendly, professionals with a passion for teaching
-													and working  with young children.
-												</p>
-											</div>
-								 </div>
-									<div class="slider-img three" id="3"/>
-								   <div class="stepUp-marquee-blueStar">
-														<div class="stepUp-marquee-stepUpMessage">
-																<h2><div class="largerLetter">S</div>tep <div class="largerLetter">U</div>p to <div class="largerLetter">Q</div>uality</h2>
-																<p>
-																		is Ohio's quality rating system administered by the Ohio Department of Education and
-																		the Ohio Department of Job and Family Services.
-																	</p>
-														</div>
-											</div>
-											<div class="stepUp-marquee-redStar">
-													<div class="stepUp-marquee-allCentersMessage">
-															<h2><div class="largerLetter">A</div>ll <div class="largerLetter">F</div>our</h2>
-															<p>
-																	Creative Child Care centers are highly rated through Step-Up-To-Quality!
-															</p>
-													</div>
-											</div>
-											<a class="stepUp-cta" href="theCreativeWay.php#creatingQualitySection">
-													Learn More
-											</a>
-											<div class="stepUp-logo-container">
-													<img src="/images/marquees/stepUpToQuality/stepUpToQuality-logo.png" />
-											</div>
-								 </div>
-						 </div>
-							<div class="sliderControls">
-								 <a href="#" class="previous" onclick="prev(); return false;"></a>
-								 <a href="#" class="next" onclick="next(); return false;"></a>
-							</div>
-		   </div>
-					<div class="clearfix"></div>
-					<div id='blurb-1' class="text-container">
-							<div class="blurb-content">
-									<h2>What makes us different</h2>
-			        <p>
-													Creative Child Care's passion is to provide affordable, high quality early educational child care utilizing developmentally appropriate standards and age appropriate curriculum that will ensure school readiness for all children in our care as they develop and learn in a safe and nurturing environment. Recognizing the parent as the first teacher, our role is to offer support through recurrent communication with the family assuring each child is exposed to positive and enriching experiences during their time with us.
-											</p>
-											<a class='orange-cta' href='theCreativeWay.php'>Learn More</a>
-									</div>
-		     </div>
-       <div id='blurb-2' class="text-container">
-							  <div class="blurb-content">
-										 <h2>Associated With</h2>
-										 <p>
-													Our expertise and participation in the Early Childhood Education Community and our knowledge of the Central Ohio Business Community makes us an asset to every child and parent we serve. Here are some of our partner organizations and programs:
-										 </p>
-									</div>
-       </div>
-							<div id="associated-logos">
-								 <a class="assoc-logo-container" href="http://www.earlychildhoodohio.org/sutq.stm" target="_blank">
-										 <img src="/images/agencyLogos/stepUpToQuality-logo-01.png" alt="StepUp to Quality Logo"/>
-								 </a>
-									<a class="assoc-logo-container" href="/documents/Building_for_the_Future_Flyer_English_FY_16.pdf" target="_blank">
-										 <img src="/images/agencyLogos/usda-logo-01.png" alt="USDA Logo"/>
-								 </a>
-									<a class="assoc-logo-container" href="http://www.acf.hhs.gov/ohs" target="_blank">
-									  <img src="/images/agencyLogos/headStart-logo-01.png" alt="Head Start Logo"/>
-									</a>
-									<a class="assoc-logo-container" href="https://education.ohio.gov/" target="_blank">
-									  <img src="/images/agencyLogos/ohioDepartmentEd-logo-01.png" alt="Ohio Department of Education Logo"/>
-									</a>
-									<a class="assoc-logo-container" href="https://www.bbb.org/centralohio/business-reviews/child-care-centers/creative-child-care-inc-in-columbus-oh-19000761/#sealclick" target="_blank" rel="nofollow">
-									  <img src="/images/agencyLogos/bbb-logo-02.png" alt="Better Business Bureau Logo"/>
-									</a>
-									<a class="assoc-logo-container" href="http://jfs.ohio.gov/ocs/" target="_blank">
-										 <img src="/images/agencyLogos/ohioDepartmentFam-logo-01.png" alt="Ohio Department of Education"/>
-									</a>
-									<a class="assoc-logo-container" href="http://earlychildhood.ehe.osu.edu/community/programs/early-head-start-partnership-program-2/" target="_blank">
-										 <img src="/images/agencyLogos/osu-logo-01.png" alt="The Ohio State University Logo"/>
-									</a>
-							</div>
-		     <div class="clearfix"></div>
-							<?php
-								include("footer.php")
-							?>
-			  <!--<div id="footer"></div>-->
-	 </div>
+  </div>
+  <div class="slider-img two" id="2"/>
+  <a id="jobs-marquee-applyNow" href="jobOpportunities.php">
+    <p>
+      Apply Now
+    </p>
+  </a>
+  <div id="jobs-marquee-joinOur">
+    <p>
+      <a href="jobOpportunities.php">Join Our Team</a>
+    </p>
+  </div>
+  <div id="jobs-marquee-ourCompany-desktop">
+    <p id="jobs-marquee-ourCompany-desktopCopy">
+      Our <strong>company</strong> is looking for energetic, friendly, professionals with a passion for teaching
+      and working  with young children.
+    </p>
+  </div>
+  <div id="jobs-marquee-ourCompany-mobile">
+    <p id="jobs-marquee-ourCompany-mobileCopy">
+      Our <strong>company</strong> is looking for energetic, friendly, professionals with a passion for teaching
+      and working  with young children.
+    </p>
+  </div>
+</div>
+<div class="slider-img three" id="3"/>
+<div class="stepUp-marquee-blueStar">
+  <div class="stepUp-marquee-stepUpMessage">
+    <h2><div class="largerLetter">S</div>tep <div class="largerLetter">U</div>p to <div class="largerLetter">Q</div>uality</h2>
+    <p>
+      is Ohio's quality rating system administered by the Ohio Department of Education and
+      the Ohio Department of Job and Family Services.
+    </p>
+  </div>
+</div>
+<div class="stepUp-marquee-redStar">
+  <div class="stepUp-marquee-allCentersMessage">
+    <h2><div class="largerLetter">A</div>ll <div class="largerLetter">F</div>our</h2>
+    <p>
+      Creative Child Care centers are highly rated through Step-Up-To-Quality!
+    </p>
+  </div>
+</div>
+<a class="stepUp-cta" href="theCreativeWay.php#creatingQualitySection">
+  Learn More
+</a>
+<div class="stepUp-logo-container">
+  <img src="/images/marquees/stepUpToQuality/stepUpToQuality-logo.png" />
+</div>
+</div>
+</div>
+<div class="sliderControls">
+  <a href="#" class="previous" onclick="prev(); return false;"></a>
+  <a href="#" class="next" onclick="next(); return false;"></a>
+</div>
+</div>
+<div class="clearfix"></div>
+<div id='blurb-1' class="text-container">
+  <div class="blurb-content">
+    <h2>What makes us different</h2>
+    <p>
+      Creative Child Care's passion is to provide affordable, high quality early educational child care utilizing developmentally appropriate standards and age appropriate curriculum that will ensure school readiness for all children in our care as they develop and learn in a safe and nurturing environment. Recognizing the parent as the first teacher, our role is to offer support through recurrent communication with the family assuring each child is exposed to positive and enriching experiences during their time with us.
+    </p>
+    <a class='orange-cta' href='theCreativeWay.php'>Learn More</a>
+  </div>
+</div>
+<div id='blurb-2' class="text-container">
+  <div class="blurb-content">
+    <h2>Associated With</h2>
+    <p>
+      Our expertise and participation in the Early Childhood Education Community and our knowledge of the Central Ohio Business Community makes us an asset to every child and parent we serve. Here are some of our partner organizations and programs:
+    </p>
+  </div>
+</div>
+<div id="associated-logos">
+  <a class="assoc-logo-container" href="http://www.earlychildhoodohio.org/sutq.stm" target="_blank">
+    <img src="/images/agencyLogos/stepUpToQuality-logo-01.png" alt="StepUp to Quality Logo"/>
+  </a>
+  <a class="assoc-logo-container" href="/documents/Building_for_the_Future_Flyer_English_FY_16.pdf" target="_blank">
+    <img src="/images/agencyLogos/usda-logo-01.png" alt="USDA Logo"/>
+  </a>
+  <a class="assoc-logo-container" href="http://www.acf.hhs.gov/ohs" target="_blank">
+    <img src="/images/agencyLogos/headStart-logo-01.png" alt="Head Start Logo"/>
+  </a>
+  <a class="assoc-logo-container" href="https://education.ohio.gov/" target="_blank">
+    <img src="/images/agencyLogos/ohioDepartmentEd-logo-01.png" alt="Ohio Department of Education Logo"/>
+  </a>
+  <a class="assoc-logo-container" href="https://www.bbb.org/centralohio/business-reviews/child-care-centers/creative-child-care-inc-in-columbus-oh-19000761/#sealclick" target="_blank" rel="nofollow">
+    <img src="/images/agencyLogos/bbb-logo-02.png" alt="Better Business Bureau Logo"/>
+  </a>
+  <a class="assoc-logo-container" href="http://jfs.ohio.gov/ocs/" target="_blank">
+    <img src="/images/agencyLogos/ohioDepartmentFam-logo-01.png" alt="Ohio Department of Education"/>
+  </a>
+  <a class="assoc-logo-container" href="http://earlychildhood.ehe.osu.edu/community/programs/early-head-start-partnership-program-2/" target="_blank">
+    <img src="/images/agencyLogos/osu-logo-01.png" alt="The Ohio State University Logo"/>
+  </a>
+</div>
+<div class="clearfix"></div>
+<?php
+include("footer.php")
+?>
+<!--<div id="footer"></div>-->
+</div>
 </body>
 </html>
